c5878ac5171f4ac66559e905bc85eccfdeec0c35
Arthur Lee <me@arthur.li>
2022-01-10 16:45:26 +0000

    Add new rcm.docxreport project

---
+++ rcm/docxreport/__main__.py
@@ -0,0 +1,5 @@
+from rcm.docxreport.generate import main
+
+
+if __name__ == "__main__":
+    main()
---
+++ rcm/docxreport/generate.py
@@ -0,0 +1,10 @@
+"""Module"""
+# pylint: disable=unused-import, missing-function-docstring
+
+from typing import Optional, Tuple, Dict, List, Any
+
+import rcm.lib
+
+import time
+
+import pandas as pd
