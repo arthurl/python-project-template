d0bdede7d887630033ed4e0e9870ec357ae1d603
Arthur Lee <git@arthur.li>
2023-06-15 11:27:58 +0100

    shell.nix: add dependencies

--- shell.nix
+++ shell.nix
@@ -1,4 +1,20 @@
 { pkgs ? import <nixpkgs> { } }:
+let my-texlive = pkgs.texlive.combine {
+     inherit (pkgs.texlive)
+       # for ipynb -> latex -> pdf conversion
+       latexmk scheme-small tcolorbox environ adjustbox collectbox titling enumitem rsfs pdfcol soul ucs
+       # for markdown template
+       csquotes xpatch inconsolata cabin fontaxes xcharter xstring newtx cleveref;
+   };
+in
+
 pkgs.mkShell {
-  buildInputs = [ pkgs.python3 pkgs.poetry ];
+  buildInputs = [
+    pkgs.python3 pkgs.poetry
+    my-texlive
+    pkgs.libxml2.dev pkgs.libxslt.dev pkgs.zlib pkgs.zeromq pkgs.nodejs
+  ];
+
+  CFLAGS = "-I${pkgs.libxml2.dev}/include/libxml2";
+  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.zlib.outPath}/lib";
 }
